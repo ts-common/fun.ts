1e5c0007b3e116d74f49db5dd1710d055f7ef1b0
Sergey Shandar <sergey.shandar@gmail.com>
2019-06-14 10:56:54 -0700

    state

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
-import * as _ from "@ts-common/iterator"
-import { CharAndPosition, Position } from "@ts-common/add-position"
+import * as _ from '@ts-common/iterator'
+import { CharAndPosition, Position } from '@ts-common/add-position'
 
-type CharSet = (c: string) => boolean
+// string => boolean
+type CharSet = (_: string) => boolean
 
-const interval = (min: string, max: string): CharSet => (c: string | null) => c !== null && min <= c && c <= max
+// string => string => CharSet
+const interval: (_: string) => (_: string) => CharSet =
+    min => max => c => min <= c && c <= max
 
-const one = (v: string): CharSet => (c: string) => v === c
+// string => CharSet
+const one: (_: string) => CharSet =
+    v => c => v === c
 
-const union = (...sets: readonly CharSet[]): CharSet => (c: string) => _.some(sets, set => set(c))
+// ...CharSet[] => CharSet
+const union = (...sets: readonly CharSet[]): CharSet => c => _.some(sets, set => set(c))
 
-const upperCaseLetterSet = interval('A', 'B')
+const upperCaseLetterSet = interval('A')('B')
 
-const lowerCaseLetterSet = interval('a', 'b')
+const lowerCaseLetterSet = interval('a')('b')
 
-const digitSet = interval('0', '9')
+const digitSet = interval('0')('9')
 
 const eSet = union(one('e'), one('E'))
 
@@ -26,38 +32,45 @@ const restIdLetterSet = union(firstIdLetterSet, digitSet)
 const whiteSpace = union(one(' '), one('\t'), one('\n'), one('\r'))
 
 type Terminal = {
-    readonly kind: "Terminal"
+    readonly kind: 'Terminal'
 }
 
 type UnknownCharacterError = {
-    readonly kind: "UnknownCharacterError"
+    readonly kind: 'UnknownCharacterError'
     readonly c: string
 }
 
-const symbolArray = ["{", "}", ":", ",", "[", "]", "-"] as const
+const symbolArray = ['{', '}', ':', ',', '[', ']', '-'] as const
 
 type ArrayItem<T> = T extends readonly (infer U)[] ? U : never
 
 type SymbolType = ArrayItem<typeof symbolArray>
 
-type Symbol = {
-    readonly kind: "Symbol"
+type SymbolToken = {
+    readonly kind: 'SymbolToken'
     readonly c: SymbolType
 }
 
-const symbolSet = (c: string): c is SymbolType => symbolArray.some(v => v === c)
+// string => SymbolType | undefined
+const symbolSet: (_: string) => SymbolType | undefined =
+    c => symbolArray.find(v => v === c)
 
 type Id = {
-    readonly kind: "Id"
+    readonly kind: 'Id'
     readonly value: string
 }
 
-type Number = {
-    readonly kind: "Number"
+type FloatNumber = {
+    readonly kind: 'FloatNumber'
     readonly value: number
 }
 
-type Token = Terminal | UnknownCharacterError | Id | Number | Symbol
+type String = {
+    readonly kind: 'String'
+    readonly value: string
+}
+
+type Token = Terminal | UnknownCharacterError | Id | FloatNumber | SymbolToken | String
 
 type TokenAndPosition = {
     readonly token: Token
@@ -66,114 +79,155 @@ type TokenAndPosition = {
 
 type StateResult = readonly [State, _.Iterable<TokenAndPosition>]
 
-type State = (c: CharAndPosition) => StateResult
+type State = (_: CharAndPosition) => StateResult
 
-const createTerminal = (position: Position): StateResult => [whiteSpaceState, [{ token: { kind: "Terminal" }, position }]]
+// Position => StateResult
+const createTerminal: (_: Position) => StateResult =
+    position => [whiteSpaceState, [{ token: { kind: 'Terminal' }, position }]]
 
-const whiteSpaceState: State = ({c, position}) =>
-    c === null
-        ? createTerminal(position)
-    : whiteSpace(c)
-        ? [whiteSpaceState, []]
-    : firstIdLetterSet(c)
-        ? [createIdState(position)(c), []]
-    : digitSet(c)
-        ? [createNumberState(position)(c), []]
-    : symbolSet(c)
-        ? [whiteSpaceState, [{ token: { kind: "Symbol", c }, position }]]
-        : [whiteSpaceState, [{ token: { kind: "UnknownCharacterError", c }, position }]]
-
-const continueWhiteSpace = (tp: TokenAndPosition, cp: CharAndPosition): StateResult => {
-    const [state, result] = whiteSpaceState(cp)
-    return [state, _.concat([tp], result)]
-}
-
-const createIdState = (position: Position) => {
-    const nextState = (value: string): State => cp => {
-        const c = cp.c
-        if (c !== null && restIdLetterSet(c)) {
-            return [nextState(value + c), []]
+// State
+const whiteSpaceState: State =
+    cp => {
+        const { c, position } = cp
+        if (c === null) {
+            return createTerminal(position)
         }
-        return continueWhiteSpace({ token: { kind: "Id", value }, position }, cp)
-    }
-    return nextState
-}
-
-const ZeroCharCode = '0'.charCodeAt(0)
-
-const charToInt = (c: string) => c.charCodeAt(0) - ZeroCharCode
-
-const createNumberState = (position: Position) => {
-
-    const beforeDot = (value: number): State => cp => {
-        const c = cp.c
-        if (c !== null) {
-            if (digitSet(c)) {
-                return [beforeDot(value * 10 + charToInt(c)), []]
-            }
-            if (c === ".") {
-                return [afterDot({ value, multiplier: 0.1 }), []]
-            }
-            if (eSet(c)) {
-                return [afterE(value), []]
-            }
+        if (whiteSpace(c)) {
+            return [whiteSpaceState, []]
         }
-        return continueWhiteSpace({ token: { kind: "Number", value }, position }, cp)
+        if (firstIdLetterSet(c)) {
+            return [createIdState(position)(c), []]
+        }
+        if (digitSet(c)) {
+            return [createNumberState(position)(c), []]
+        }
+        const symbol = symbolSet(c)
+        if (symbol !== undefined) {
+            return [whiteSpaceState, [{ token: { kind: 'SymbolToken', c: symbol }, position }]]
+        }
+        if (c === '"') {
+            return [createStringState(position), []]
+        }
+        return [whiteSpaceState, [{ token: { kind: 'UnknownCharacterError', c }, position }]]
     }
 
-    const afterDot = ({ value, multiplier }: NumberAfterDot) => (cp: CharAndPosition): StateResult => {
-        const { c } = cp
-        if (c !== null) {
-            if (digitSet(c)) {
-                return [afterDot({ value: value + charToInt(c) * multiplier, multiplier: multiplier * 0.1 }), []]
-            }
-            if (eSet(c)) {
-                return [afterE(value), []]
-            }
-        }
-        return continueWhiteSpace({ token: { kind: "Number", value }, position }, cp)
+// TokenAndPosition => State
+const continueWhiteSpace: (_: TokenAndPosition) => State =
+    tp => cp => {
+        const [state, result] = whiteSpaceState(cp)
+        return [state, _.concat([tp], result)]
     }
 
-    const afterE = (value: number) => (cp: CharAndPosition): StateResult => {
-        const { c } = cp
-        if (c !== null) {
-            if (c === '-') {
-                return [afterESign({ value, positive: false }), []]
+// Position => string => State
+const createIdState: (_: Position) => (_: string) => State =
+    position => {
+        // string => State
+        const nextState: (value: string) => State =
+            value => cp => {
+                const { c } = cp
+                return c !== null && restIdLetterSet(c)
+                    ? [nextState(value + c), []]
+                    : continueWhiteSpace({ token: { kind: 'Id', value }, position })(cp)
             }
-            const plus = afterESign({ value, positive: true })
-            if (c === '+') {
-                return [plus, []]
-            }
-            if (digitSet(c)) {
-                return plus(cp)
-            }
-        }
-        return continueWhiteSpace({ token: { kind: "Number", value }, position }, cp)
+        return nextState
     }
 
-    // {value}E+23 = {value} * 10 ** 23 => 10 ** (20 + 3) => {value} * ((10 ** 2) ** 10) * (10 ** 3)
-    const afterESign = ({ value, positive }: NumberAfterESign) => {
-        const state = (multiplier: number) => (cp: CharAndPosition): StateResult => {
-            const { c } = cp
-            if (c !== null) {
-                if (digitSet(c)) {
-                    return [state(multiplier ** 10 * multiplier * 10 ** charToInt(c)), []]
+const zeroCharCode = '0'.charCodeAt(0)
+
+// string => number
+const charToInt: (_: string) => number =
+    c => c.charCodeAt(0) - zeroCharCode
+
+// Position => string => State
+const createNumberState: (_: Position) => (_: string) => State =
+    position => {
+
+        // number => State
+        const end: (_: number) => State =
+            value => continueWhiteSpace({ token: { kind: 'FloatNumber', value }, position })
+
+        // number => State
+        const begin: (_: number) => State =
+            value => cp => {
+                const { c } = cp
+                if (c !== null) {
+                    if (digitSet(c)) {
+                        return [begin(value * 10 + charToInt(c)), []]
+                    }
+                    if (c === '.') {
+                        return [afterDot(value)(0.1), []]
+                    }
+                    if (eSet(c)) {
+                        return [afterE(value), []]
+                    }
                 }
+                return end(value)(cp)
             }
-            return continueWhiteSpace({ token: { kind: "Number", value: value * (positive ? multiplier : 1 / multiplier) }, position }, cp)
-        }
-        return state(1)
+
+        // number => number => State
+        const afterDot: (_: number) => (_: number) => State =
+            value => multiplier => cp => {
+                const { c } = cp
+                if (c !== null) {
+                    if (digitSet(c)) {
+                        return [afterDot(value + charToInt(c) * multiplier)(multiplier * 0.1), []]
+                    }
+                    if (eSet(c)) {
+                        return [afterE(value), []]
+                    }
+                }
+                return end(value)(cp)
+            }
+
+        // number => State
+        const afterE: (_: number) => State =
+            value => cp => {
+                const { c } = cp
+                if (c !== null) {
+                    if (c === '-') {
+                        return [afterESign(value)(false), []]
+                    }
+                    const plus = afterESign(value)(true)
+                    if (c === '+') {
+                        return [plus, []]
+                    }
+                    if (digitSet(c)) {
+                        return plus(cp)
+                    }
+                }
+                return end(value)(cp)
+            }
+
+        // number => boolean => State
+        const afterESign: (_: number) => (_: boolean) => State =
+            value => positive => {
+                // number => State
+                const multiplierState: (_: number) => State =
+                    multiplier => cp => {
+                        const { c } = cp
+                        if (c !== null && digitSet(c)) {
+                            // eg.: E+23 = 10 ** 23 = (10 ** 2) ** 10 * 10 ** 3
+                            return [multiplierState(multiplier ** 10 * 10 ** charToInt(c)), []]
+                        }
+                        return end(value * (positive ? multiplier : 1 / multiplier))(cp)
+                    }
+                return multiplierState(1)
+            }
+
+        return c => begin(charToInt(c))
     }
 
-    return (c: string) => beforeDot(charToInt(c))
-}
-
-type NumberAfterDot = {
-    readonly value: number
-    readonly multiplier: number
-}
-
-type NumberAfterESign = {
-    readonly value: number
-    readonly positive: boolean
-}
+// Position => State
+const createStringState: (_: Position) => State =
+    position => {
+        // string => State
+        const state: (_: string) => State =
+            value => cp => {
+                const { c } = cp
+                if (c === null || c === '\n' || c === '"') {
+                    return continueWhiteSpace({ token: { kind: 'String', value }, position })(cp)
+                }
+                return [state(value + c), []]
+            }
+        return state('')
+    }
