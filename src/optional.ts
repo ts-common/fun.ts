24678afe6da8e2194ee8024ba70661d9f96763fc
Denis Fuenzalida <defuenza@microsoft.com>
2019-07-23 17:06:05 -0700

    TSDocs for equal, optional, predicate, sign

--- src/optional.ts
+++ src/optional.ts
@@ -1,5 +1,13 @@
+/**
+ * Optional is either a value of a type `T` or `undefined`
+ */
 export type Optional<T> = T | undefined
 
+/**
+ * @param f a function from a type T to a type R
+ * @param optional an `Optional` value of type `T` or `undefined`
+ * @returns the result of applying f to `optional` if it's defined, otherwise undefined.
+ */
 export const map
     : <T, R>(_: (_: T) => R) => (_: Optional<T>) => Optional<R>
     = f => optional => optional === undefined ? undefined : f(optional)
